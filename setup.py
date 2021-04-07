7e0092678b0f09832e421352a1e6e37ada400739
Aldo Gunsing <dev@aldogunsing.nl>
2021-04-07 22:03:44 +0200

    Bump version to 1.2.1

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name="radicale_storage_decsync",
-    version="1.2.0",
+    version="1.2.1",
     author="Aldo Gunsing",
     author_email="dev@aldogunsing.nl",
     url="https://github.com/39aldo39/Radicale-DecSync",
